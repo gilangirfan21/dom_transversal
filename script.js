769a759a788b146adcd33aff551c57ca02ffad2c
gilangirfan21 <gilangxirfan21@gmail.com>
2021-02-18 14:51:28 +0700

    penggunaan dom transversal pertama

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-const tombolClose = document.querySelector(".close");
-const card = document.querySelector(".card");
+// const tombolClose = document.querySelector(".close");
+// const card = document.querySelector(".card");
 
 
-tombolClose.addEventListener("click",function(){
-    card.style.display = "none";
-});
+// tombolClose.addEventListener("click",function(){
+//     card.style.display = "none";
+// });
 
 
+const close = document.querySelectorAll(".close");
+// const card = document.querySelectorAll(".card");
+
+for (let i = 0 ; i < close.length ; i++){
+    close[i].addEventListener("click",function(){
+        // card[i].style.display = "none";
+        close[i].parentElement.style.display = "none"; //dom transversal
+    });
+}
