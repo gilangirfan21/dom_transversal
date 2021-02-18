d18a4e981434eb7ae3f95f44425ddcd67962b59b
gilangirfan21 <gilangxirfan21@gmail.com>
2021-02-18 15:55:11 +0700

    update dom transversal

--- script.js
+++ script.js
@@ -10,9 +10,20 @@
 const close = document.querySelectorAll(".close");
 // const card = document.querySelectorAll(".card");
 
-for (let i = 0 ; i < close.length ; i++){
-    close[i].addEventListener("click",function(){
-        // card[i].style.display = "none";
-        close[i].parentElement.style.display = "none"; //dom transversal
+
+//ini menggunakan for
+// for (let i = 0 ; i < close.length ; i++){
+//     close[i].addEventListener("click",function(e){
+//         // card[i].style.display = "none";
+//         // close[i].parentElement.style.display = "none"; //dom transversal
+//         e.target.parentElement.style.display = "none"; //memanfaatkan e (informasi event yang sedang terjadi)
+//     });
+// }
+
+
+//ini menggunakan forEach
+close.forEach(function(el){
+    el.addEventListener("click",function(e){
+        e.target.parentElement.style.display = "none";
     });
-}
+});
