b41a79a79e204abb164af9a06dfb234711423eca
gilangirfan21 <gilangxirfan21@gmail.com>
2021-02-18 19:21:40 +0700

    belajar event bubble

--- script.js
+++ script.js
@@ -7,24 +7,33 @@
 // });
 
 
-const close = document.querySelectorAll(".close");
-// const card = document.querySelectorAll(".card");
+// const close = document.querySelectorAll(".close");
+// // const card = document.querySelectorAll(".card");
 
 
-//ini menggunakan for
-// for (let i = 0 ; i < close.length ; i++){
-//     close[i].addEventListener("click",function(e){
-//         // card[i].style.display = "none";
-//         // close[i].parentElement.style.display = "none"; //dom transversal
-//         e.target.parentElement.style.display = "none"; //memanfaatkan e (informasi event yang sedang terjadi)
+// //ini menggunakan for
+// // for (let i = 0 ; i < close.length ; i++){
+// //     close[i].addEventListener("click",function(e){
+// //         // card[i].style.display = "none";
+// //         // close[i].parentElement.style.display = "none"; //dom transversal
+// //         e.target.parentElement.style.display = "none"; //memanfaatkan e (informasi event yang sedang terjadi)
+// //     });
+// // }
+
+
+// //ini menggunakan forEach
+// close.forEach(function(el){
+//     el.addEventListener("click",function(e){
+//         e.target.parentElement.style.display = "none";
+//         e.preventDefault(); //menghilangkan / mencegah menjalankan default action (prevent default)
 //     });
-// }
+// });
 
+//membuat script lebih sederhana dengan konsep Event bubble 
+const container = document.querySelector(".container");
 
-//ini menggunakan forEach
-close.forEach(function(el){
-    el.addEventListener("click",function(e){
+container.addEventListener("click",function(e){
+    if(e.target.className == "close"){
         e.target.parentElement.style.display = "none";
-        e.preventDefault(); //menghilangkan / mencegah menjalankan default action (prevent default)
-    });
+    }
 });
